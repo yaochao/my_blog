ec2e239c14c310389a1bfdb8d0bb98b4dec3c43a
yaochao <yaochao365@qq.com>
2017-04-04 16:52:50 +0800

    add views.py and urls.py, and got a custom page map to custom url

--- article/views.py
+++ article/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from article.models import ArticleModel
+
+def home(request):
+    return HttpResponse('Hello world, Django!')
+
+def detail(request, param):
+    return HttpResponse('detail parameter: %s' % param)
+
+def article(request, id):
+    a1 = ArticleModel.objects.get(id=int(id))
+    return HttpResponse('title: %s<br>content: %s<br>' % (a1.title, a1.content))
