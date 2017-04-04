cf67b45a5877e43c5fbc6118e3b7c60bbe41d56a
yaochao <yaochao365@qq.com>
2017-04-04 14:27:08 +0800

    add a model

--- article/models.py
+++ article/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ArticleModel(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True) # auto_now_add: 自动设置对象增加时间
+    content = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date_time'] # 按时间降序排序
